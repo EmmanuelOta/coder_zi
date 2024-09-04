1eae50922b7f04e5a3c75b04bde1116edbdffe7d
Emmanuel <65783304+EmmanuelOta@users.noreply.github.com>
2024-09-04 16:26:43 +0100

    Updating navbar code-4

--- app/components/Footer/Footer.js
+++ app/components/Footer/Footer.js
@@ -8,7 +8,7 @@ const libre_baskerville = Libre_Baskerville({
 
 export default function Footer() {
 	return (
-		<nav className="flex items-center z-50 border-t border-t-slate-300 p-4 bg-gradient-to-r from-slate-50 to-slate-200 p-5">
+		<nav className="flex items-center z-50 border-t border-t-slate-300 p-4 bg-gradient-to-r from-slate-100 via-white to-slate-200 dark:bg-gradient-to-b dark:from-zinc-900 dark:to-slate-950 p-5">
 			<div className="flex flex-col lg:flex-row lg:items-center justify-between w-full">
 				<div>
 					<div className="flex flex-row items-end">
@@ -42,7 +42,7 @@ export default function Footer() {
 							viewBox="0 0 1200 1227"
 							fill="none"
 							xmlns="http://www.w3.org/2000/svg"
-							class="w-[14px] h-[14px]"
+							class="w-[14px] h-[14px] dark:invert"
 						>
 							<path
 								d="M714.163 519.284L1160.89 0H1055.03L667.137 450.887L357.328 0H0L468.492 681.821L0 1226.37H105.866L515.491 750.218L842.672 1226.37H1200L714.137 519.284H714.163ZM569.165 687.828L521.697 619.934L144.011 79.6944H306.615L611.412 515.685L658.88 583.579L1055.08 1150.3H892.476L569.165 687.854V687.828Z"
@@ -63,6 +63,7 @@ export default function Footer() {
 							width="20"
 							height="20"
 							viewBox="0 0 50 50"
+							className="dark:invert"
 						>
 							<path d="M25,3C12.85,3,3,12.85,3,25c0,11.03,8.125,20.137,18.712,21.728V30.831h-5.443v-5.783h5.443v-3.848 c0-6.371,3.104-9.168,8.399-9.168c2.536,0,3.877,0.188,4.512,0.274v5.048h-3.612c-2.248,0-3.033,2.131-3.033,4.533v3.161h6.588 l-0.894,5.783h-5.694v15.944C38.716,45.318,47,36.137,47,25C47,12.85,37.15,3,25,3z"></path>
 						</svg>
@@ -74,6 +75,7 @@ export default function Footer() {
 							width={20}
 							height={20}
 							alt="mail support"
+							className="dark:invert"
 						/>
 					</a>
 				</div>
