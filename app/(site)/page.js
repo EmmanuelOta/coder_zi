6e7fa8461174d43d8cf99ad9a6366e1e939f1119
Emmanuel <65783304+EmmanuelOta@users.noreply.github.com>
2024-09-06 18:30:31 +0100

    Adding a swiper for projects section-1

--- app/(site)/page.js
+++ app/(site)/page.js
@@ -131,7 +131,7 @@ export default function Home() {
 							Wanderlust
 						</h4>
 						<p className="p-2 font-medium">
-							Wanderlust is a vacation destination, with over 200
+							Wanderlust is a vacation destination site, with over 200
 							vacation destinations ready for tourist to explore
 						</p>
 					</div>
