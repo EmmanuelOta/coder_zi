9b577344628a087d40cfb2161cc83b6ea5956a60
Emmanuel <65783304+EmmanuelOta@users.noreply.github.com>
2024-09-08 04:38:26 -1200

    Adding more UI to the homepage-1

--- app/(site)/page.js
+++ app/(site)/page.js
@@ -38,6 +38,14 @@ export default function Home() {
 		"emporion-withdraw.png",
 	];
 
+	const revia = [
+		"revia-hero.png",
+		"revia-benefits.png",
+		"revia-how-it-works.png",
+		"revia-steps.png",
+		"revia-faq.png",
+	];
+
 	return (
 		<>
 			<section className="flex flex-col items-center justify-center p-5">
@@ -150,7 +158,7 @@ export default function Home() {
 			</section>
 
 			<section
-				className="flex flex-col items-center justify-center p-5"
+				className="flex flex-col items-center justify-center"
 				id="projects"
 			>
 				<h3
@@ -263,14 +271,14 @@ export default function Home() {
 							Trading Exchange Patform
 						</h4>
 						<p className="p-2 font-medium">
-							Emporion is a mock trading platform
-							designed to simulate real-world trading. Featuring a
-							sleek landing page, a dynamic dashboard for
-							executing trades, and real-time user data updates.
-							It also features a beautiful coin marquee at the top
-							of the page that showcases live crypto prices. Built
-							with Next.js and tailwind for the front end and
-							Python Flask for the backend, it includes a secure
+							Emporion is a mock trading platform designed to
+							simulate real-world trading. Featuring a sleek
+							landing page, a dynamic dashboard for executing
+							trades, and real-time user data updates. It also
+							features a beautiful coin marquee at the top of the
+							page that showcases live crypto prices. Built with
+							Next.js and tailwind for the front end and Python
+							Flask for the backend, it includes a secure
 							authentication system and showcases my ability to
 							develop both user-focused interfaces and robust
 							backend services.
@@ -296,6 +304,55 @@ export default function Home() {
 								/>
 							</svg>
 						</a>
+					</div>
+				</div>
+
+				<div className="flex flex-col lg:flex-row items-center justify-center max-w-full my-5 lg:my-10">
+					<div
+						className="w-full lg:w-[50%] p-4 mx-0 lg:mx-5"
+						data-aos="fade-left"
+					>
+						<h4 className="text-2xl font-semibold text-center">
+							AI Voice Call Assistant (Revia) - Landing Page
+						</h4>
+						<p className="p-2 font-medium">
+							Revia is a sleek landing page for an AI voice call
+							assistant, built with Next.js and Tailwind CSS. The
+							design is clean, intuitive, and optimized for
+							conversions, showcasing my ability to apply design
+							principles that engage users. With compelling copy
+							and smooth animations, I crafted an interface that
+							not only looks great but effectively drives user
+							interaction and conversions.
+						</p>
+						<a
+							target="_blank"
+							href="https://revia.vercel.app"
+							className="mx-1 inline-flex items-center font-semibold p-2"
+						>
+							Revia
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								strokeWidth={1.5}
+								stroke="currentColor"
+								className="size-3 mx-1"
+							>
+								<path
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+								/>
+							</svg>
+						</a>
+					</div>
+
+					<div
+						className="w-full lg:w-[50%] p-4"
+						data-aos="fade-right"
+					>
+						<Slider pictures={revia} />
 					</div>
 				</div>
 			</section>
